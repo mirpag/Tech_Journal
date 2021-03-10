9f9f59a243037cf79d3e169bffdd6ad7d62b4e47
mirpag <mpagarelski@gmail.com>
2021-03-09 21:36:51 -0500

    added a script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,19 @@
+#secure-ssh.sh
+#author mirpag
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "Running script..." 
+
+echo "ADD USER"
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+echo "mkdir"
+sudo mkdir /home/$1/.ssh
+echo "copy pub key"
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+echo "chmod .ssh"
+sudo chmod 700 /home/$1/.ssh
+echo "chmod .ssh/authorized_keys"
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+echo "chown .ssh"
+sudo chown -R $1:$1 /home/$1/.ssh
